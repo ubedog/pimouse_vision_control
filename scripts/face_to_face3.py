cb5528e6750c0e0086517c7f4aa82d4efb745582
takata okada <aozoratakata@gmail.com>
2021-01-26 19:12:14 +0900

    コミットした

--- scripts/face_to_face3.py
+++ scripts/face_to_face3.py
@@ -6,17 +6,16 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class FaceToFace():
     def __init__(self):
-        sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.get_image)
-        self.pub = rospy.Publisher("face", Image, queue_size=1)
+        sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.get_image)        self.pub = rospy.Publisher("face", Image, queue_size=1)
         self.bridge = CvBridge()
         self.image_org = None
 
-    def monitor(self,rect,org):                                 #このメソッドを追加
+    def monitor(self,rect,org):
         if rect is not None:
             cv2.rectangle(org,tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]),(0,255,255),4)
-       
+
         self.pub.publish(self.bridge.cv2_to_imgmsg(org, "bgr8"))
-   
+
     def get_image(self,img):
         try:
             self.image_org = self.bridge.imgmsg_to_cv2(img, "bgr8")
@@ -26,27 +25,28 @@ class FaceToFace():
     def detect_face(self):
         if self.image_org is None:
             return None
-    
+
         org = self.image_org
-    
+
         gimg = cv2.cvtColor(org,cv2.COLOR_BGR2GRAY)
         classifier = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
         cascade = cv2.CascadeClassifier(classifier)
         face = cascade.detectMultiScale(gimg,1.1,1,cv2.CASCADE_FIND_BIGGEST_OBJECT)
-    
-        if len(face) == 0:    #len(face)...以下を次のように書き換え
+
+        if len(face) == 0:
             self.monitor(None,org)
-            return None       
-                              
-        r = face[0]           
-        self.monitor(r,org)   
-        return r  
+            return None
+
+        r = face[0]
+        self.monitor(r,org)
+        return r
 
 if __name__ == '__main__':
     rospy.init_node('face_to_face')
     fd = FaceToFace()
 
-    rate = rospy.Rate(10)
+    rate =rospy.Rate(10)
     while not rospy.is_shutdown():
         rospy.loginfo(fd.detect_face())
         rate.sleep()
+
