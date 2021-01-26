bd2112c2d2c25e3bd3a89609edb8f9002c882eda
takata okada <aozoratakata@gmail.com>
2021-01-26 18:23:06 +0900

    コミットしました

--- scripts/face_to_face2.py
+++ scripts/face_to_face2.py
@@ -12,26 +12,26 @@ class FaceToFace():
 
     def get_image(self,img):
         try:
-            self.image_org = self.bridge.imgmsg_to_cv2(img, "bgr8")
+            self.image_org = self.bridge.image_to_cv2(img, "bgr8")
         except CvBridgeError as e:
             rospy.logerr(e)
 
     def detect_face(self):
         if self.image_org is None:
             return None
-    
+
         org = self.image_org
-    
+
         gimg = cv2.cvtColor(org,cv2.COLOR_BGR2GRAY)
-        classifier = "/usr/share/opencv/haacascades/haarcascade_frontalface_default.xml"
+        classifier = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
         cascade = cv2.CascadeClassifier(classifier)
         face = cascade.detectMultiScale(gimg,1.1,1,cv2.CASCADE_FIND_BIGGEST_OBJECT)
-    
+
         if len(face) == 0:
             return None
-    
+
         r = face[0]
-        cv2.rectangle(org,tuple(r[0:2]),tuple(r[0:2]+r[2:4]),(0,255,255),4)
+        cv2.rectangel(org,tuple(r[0:2]),tuple(r[0:2]+r[2:4]),(0,255,255),4)
         cv2.imwrite("/tmp/image.jpg",org)
         return "detected"
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     rospy.init_node('face_to_face')
     fd = FaceToFace()
 
-    rate = rospy.Rate(10)
+    rate =rospy.Rate(10)
     while not rospy.is_shutdown():
         rospy.loginfo(fd.detect_face())
         rate.sleep()
